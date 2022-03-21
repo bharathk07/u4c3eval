257f3e177f8c0586691cb44ad85a59aaffeb6162
bharathk07 <97445865+bharathk07@users.noreply.github.com>
2022-03-21 11:21:23 +0530

    eval

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const app = express();
 app.use(express.json());
 const {register,login }= require("./controller/auth.controller");
 const {body, validationResult} = require("express-validator");
-const commentController = require("./controller/comment.controller")
+const commentController = require("./controller/comment.controller");
+const bookController = require("./controller/book.controller");
 
 //validation of the user registering
 app.post("/register",
   body("firstName").not().isEmpty().isLength({min:3, max:30}).withMessage("min 3 and max 30 chars allowed"),
   body("lastName").not().isEmpty().isLength({min:3, max:30}).withMessage("min 3 and max 30 chars allowed"),
-  body("age").isLength({min:1,max:3}).withMessage("No man has lived beyond 135 years").custom((value)=>{
+  body("age").withMessage("No man has lived beyond 135 years").custom((value)=>{
     if(value<1 || value >100){
         throw new Error("Age should between 1 to 100");
     }
@@ -21,7 +22,7 @@ app.post("/register",
 app.post("/login",
   body("firstName").not().isEmpty().isLength({min:3, max:30}).withMessage("min 3 and max 30 chars allowed"),
   body("lastName").not().isEmpty().isLength({min:3, max:30}).withMessage("min 3 and max 30 chars allowed"),
-  body("age").isLength({min:1,max:3}).withMessage("No man has lived beyond 135 years").custom((value)=>{
+  body("age").withMessage("No man has lived beyond 135 years").custom((value)=>{
     if(value<1 || value >100){
         throw new Error("Age should between 1 to 100");
     }
@@ -30,9 +31,8 @@ app.post("/login",
 
 );
 //create a post route to create comments.
-app.use("/comment", commentController)
-
-
-
+app.use("/comment", commentController);
+//create a post route to create books with 1 cover image.
+app.use("/book", bookController);
 
 module.exports = app;
